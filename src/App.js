22fe8f03b9fdabc0bc176ad15191aaf11ee4bea6
Vince Guo <vince.guoo@gmail.com>
2024-12-01 20:13:43 -0500

    Adds Proected route and admin route

--- src/App.js
+++ src/App.js
@@ -11,26 +11,34 @@ import AuthRoute from './Utilities/authRoute'
 import AboutPage from './App/About/AboutPage';
 import NewCompanyForm from './App/Company/NewCompanyForm';
 import NewJobForm from './App/JobPosts/NewJobForm';
+import { EmployeePage } from './App/Admin/EmployeePage';
+import ProtectedRoute from './Utilities/authRoute';
+import AdminRoute from './Utilities/adminRoute';
 
 function App() {
   return (
     <div>
       <MyNavbar />
+
       <Routes>
-        <Route path="/job-listing" element={<JobListing />}/>
-        <Route path="/job-listing/new-job-form" element={<NewJobForm />}/>
+          {/* <Route path="/about" element={<AboutPage />} /> */}
+              <Route element={<ProtectedRoute/>}>
+              <Route path="/job-listing" element={<JobListing />}/>
+              <Route path="/job-listing/new-job-form" element={<NewJobForm />}/>
+              <Route element={<AdminRoute/>}>
+                <Route path="/employees" element={<EmployeePage />}/>
+              </Route>
+              <Route path="/contact" element={<ContactPage />} />
+              <Route path="/company-showcase" element={<CompanyShowCasePage />} />
+              {/* <Route path="new-company-form" element={<h1>TESTING</h1>} /> */}
+    
+              <Route path="/company-showcase/new-company-form" element={<NewCompanyForm />} />
+              <Route path="/user-favorited-page" element={<FavoritedJobList/>} />
+              <Route path="/company-showcase-details/:id" element={<CompanyAboutPage />}/>
 
-        {/* <Route path="/about" element={<AboutPage />} /> */}
-        <Route path="/contact" element={<ContactPage />} />  
-        <Route path="/company-showcase" element={<CompanyShowCasePage />} />
-          {/* <Route path="new-company-form" element={<h1>TESTING</h1>} /> */}
-  
-        <Route path="/company-showcase/new-company-form" element={<NewCompanyForm />} />
-        <Route path="/user-favorited-page" element={<FavoritedJobList/>} />
-        <Route path="/company-showcase-details/:id" element={<CompanyAboutPage />}/>
-
-        <Route path="/about" element={<AboutPage />}/>
-        <Route path="/home" element={<LoginPage />}/> {/* 👈 Renders at /app/ */}
+              <Route path="/about" element={<AboutPage />}/>  
+          </Route>
+          <Route path="/home" element={<LoginPage />}/> {/* 👈 Renders at /app/ */}
 
       </Routes>
     </div>
--- src/Utilities/authRoute.js
+++ src/Utilities/authRoute.js
@@ -1,12 +1,26 @@
-import { Navigate } from 'react-router-dom';
 import { useAuth } from './authProvider';
+import React from 'react';
+import { Route, Navigate, Outlet } from 'react-router-dom';
 
-const PrivateRoute = ({children, redirectTo }) => {
-    const {isLoggedIn, actions} = useAuth()
+// const PrivateRoute = ({children, redirectTo }) => {
+//     const {isLoggedIn, actions} = useAuth()
+//     if(!isLoggedIn.loggedIn) {
+//         console.log("NOT LOGGED IN");
+//     }
+//     //If the user is logged in, then allow navigation
+//     //If user is not logged in, then redirect to sign in page
+//     return isLoggedIn.isLoggedIn ? children : <Navigate to='/home'  /> 
+// }
 
-    //If the user is logged in, then allow navigation
-    //If user is not logged in, then redirect to sign in page
-    return isLoggedIn.isLoggedIn ? {...children} : <Navigate to='/home'  /> 
-}
+// export default PrivateRoute
+const ProtectedRoute = () => {
+    const { isLoggedIn } = useAuth();
 
-export default PrivateRoute
+    if(!isLoggedIn.loggedIn) {
+        return <Navigate to="/home" replace />
+    }
+
+    return <Outlet />
+};
+
+export default ProtectedRoute;
